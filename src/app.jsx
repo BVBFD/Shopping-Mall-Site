27839b6e2984848d325d56055f6b9fe146e68ce1
BVBFD <lsevina126@gmail.com>
2021-12-22 22:22:06 +0900

    Code Featured Products part of Products page

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import About from "./components/aboutPart/about";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import Products from "./components/products/products";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route exact={true} path={"/"} element={<Home />} />
         <Route exact={true} path={"/about"} element={<About />} />
+        <Route exact={true} path={"/products"} element={<Products />} />
       </Routes>
       <Footer />
     </>
---
+++ src/components/products/products.jsx
@@ -0,0 +1,394 @@
+import React from "react";
+import styles from "./products.module.css";
+
+const Products = (props) => {
+  return (
+    <section className={styles.products}>
+      <h1 className={styles.heading}>
+        Featured <span>Products</span>
+      </h1>
+
+      <div className={styles.boxContainer}>
+        <div className={styles.box}>
+          <div className={styles.image}>
+            <img
+              src="./images/product-1.jpg"
+              className={styles.mainImg}
+              alt=""
+            />
+            <img
+              src="./images/product-1-hover.jpg"
+              className={styles.hoverImg}
+              alt=""
+            />
+            <div className={styles.icons}>
+              <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-search-plus"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-heart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-share"></i>
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <h3>Smart Phone</h3>
+            <div className={styles.price}>
+              $249.99 <span>$399.99</span>
+            </div>
+            <div className={styles.stars}>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <div className={styles.image}>
+            <img
+              src="./images/product-1.jpg"
+              className={styles.mainImg}
+              alt=""
+            />
+            <img
+              src="./images/product-1-hover.jpg"
+              className={styles.hoverImg}
+              alt=""
+            />
+            <div className={styles.icons}>
+              <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-search-plus"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-heart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-share"></i>
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <h3>Smart Phone</h3>
+            <div className={styles.price}>
+              $249.99 <span>$399.99</span>
+            </div>
+            <div className={styles.stars}>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <div className={styles.image}>
+            <img
+              src="./images/product-1.jpg"
+              className={styles.mainImg}
+              alt=""
+            />
+            <img
+              src="./images/product-1-hover.jpg"
+              className={styles.hoverImg}
+              alt=""
+            />
+            <div className={styles.icons}>
+              <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-search-plus"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-heart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-share"></i>
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <h3>Smart Phone</h3>
+            <div className={styles.price}>
+              $249.99 <span>$399.99</span>
+            </div>
+            <div className={styles.stars}>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <div className={styles.image}>
+            <img
+              src="./images/product-1.jpg"
+              className={styles.mainImg}
+              alt=""
+            />
+            <img
+              src="./images/product-1-hover.jpg"
+              className={styles.hoverImg}
+              alt=""
+            />
+            <div className={styles.icons}>
+              <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-search-plus"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-heart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-share"></i>
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <h3>Smart Phone</h3>
+            <div className={styles.price}>
+              $249.99 <span>$399.99</span>
+            </div>
+            <div className={styles.stars}>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <div className={styles.image}>
+            <img
+              src="./images/product-1.jpg"
+              className={styles.mainImg}
+              alt=""
+            />
+            <img
+              src="./images/product-1-hover.jpg"
+              className={styles.hoverImg}
+              alt=""
+            />
+            <div className={styles.icons}>
+              <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-search-plus"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-heart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-share"></i>
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <h3>Smart Phone</h3>
+            <div className={styles.price}>
+              $249.99 <span>$399.99</span>
+            </div>
+            <div className={styles.stars}>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <div className={styles.image}>
+            <img
+              src="./images/product-1.jpg"
+              className={styles.mainImg}
+              alt=""
+            />
+            <img
+              src="./images/product-1-hover.jpg"
+              className={styles.hoverImg}
+              alt=""
+            />
+            <div className={styles.icons}>
+              <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-search-plus"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-heart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-share"></i>
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <h3>Smart Phone</h3>
+            <div className={styles.price}>
+              $249.99 <span>$399.99</span>
+            </div>
+            <div className={styles.stars}>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <div className={styles.image}>
+            <img
+              src="./images/product-1.jpg"
+              className={styles.mainImg}
+              alt=""
+            />
+            <img
+              src="./images/product-1-hover.jpg"
+              className={styles.hoverImg}
+              alt=""
+            />
+            <div className={styles.icons}>
+              <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-search-plus"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-heart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-share"></i>
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <h3>Smart Phone</h3>
+            <div className={styles.price}>
+              $249.99 <span>$399.99</span>
+            </div>
+            <div className={styles.stars}>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <div className={styles.image}>
+            <img
+              src="./images/product-1.jpg"
+              className={styles.mainImg}
+              alt=""
+            />
+            <img
+              src="./images/product-1-hover.jpg"
+              className={styles.hoverImg}
+              alt=""
+            />
+            <div className={styles.icons}>
+              <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-search-plus"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-heart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-share"></i>
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <h3>Smart Phone</h3>
+            <div className={styles.price}>
+              $249.99 <span>$399.99</span>
+            </div>
+            <div className={styles.stars}>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <div className={styles.image}>
+            <img
+              src="./images/product-1.jpg"
+              className={styles.mainImg}
+              alt=""
+            />
+            <img
+              src="./images/product-1-hover.jpg"
+              className={styles.hoverImg}
+              alt=""
+            />
+            <div className={styles.icons}>
+              <a href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-search-plus"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-heart"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-share"></i>
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <h3>Smart Phone</h3>
+            <div className={styles.price}>
+              $249.99 <span>$399.99</span>
+            </div>
+            <div className={styles.stars}>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Products;
